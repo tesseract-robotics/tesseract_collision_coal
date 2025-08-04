d68123ae3fd9d256ba3589be3371debafef1d729
Roelof Oomen <rj.oomen@isohorti.com>
2025-08-04 09:46:46 +0200

    Fix double lookup of collision objects

--- tesseract_collision/coal/src/coal_discrete_managers.cpp
+++ tesseract_collision/coal/src/coal_discrete_managers.cpp
@@ -96,16 +96,14 @@ bool CoalDiscreteBVHManager::addCollisionObject(const std::string& name,
 const CollisionShapesConst& CoalDiscreteBVHManager::getCollisionObjectGeometries(const std::string& name) const
 {
   auto cow = link2cow_.find(name);
-  return (link2cow_.find(name) != link2cow_.end()) ? cow->second->getCollisionGeometries() :
-                                                     EMPTY_COLLISION_SHAPES_CONST;
+  return (cow != link2cow_.end()) ? cow->second->getCollisionGeometries() : EMPTY_COLLISION_SHAPES_CONST;
 }
 
 const tesseract_common::VectorIsometry3d&
 CoalDiscreteBVHManager::getCollisionObjectGeometriesTransforms(const std::string& name) const
 {
   auto cow = link2cow_.find(name);
-  return (link2cow_.find(name) != link2cow_.end()) ? cow->second->getCollisionGeometriesTransforms() :
-                                                     EMPTY_COLLISION_SHAPES_TRANSFORMS;
+  return (cow != link2cow_.end()) ? cow->second->getCollisionGeometriesTransforms() : EMPTY_COLLISION_SHAPES_TRANSFORMS;
 }
 
 bool CoalDiscreteBVHManager::hasCollisionObject(const std::string& name) const
